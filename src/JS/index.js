440258b2da069f5997edb32a1ea51c8e715c8017
MrHeimdall <sergio_prat_22@hotmail.com>
2021-05-23 03:39:46 +0200

    Edit and delete feature, changes in css

--- src/JS/index.js
+++ src/JS/index.js
@@ -1,50 +1,64 @@
 
 const addButton = document.getElementById("add-button");
 const todoList = document.getElementById("todo-list")
-  
-
 addButton.addEventListener("click", addTodo);
- 
-
 
 let index = 0;
 
 function addTodo(){
   const todoInput = document.getElementById('todo-input');
+
   if(!todoInput.value){
     console.log("tarea vacía");
     return;
   }
 
-  createTodo(todoInput.value);
+  function createTodo (value){
+
+    const taskContainer = document.createElement('div')
+    const task = document.createElement('div');
+    
+    todoList.appendChild(taskContainer);
+    task.classList.add('todo-list')
+        
+
+    const taskLabel = document.createElement('li');
+    taskLabel.innerText = value;
+
+    const editButtton = document.createElement('button');
+    editButtton.innerText ='edit'
+    editButtton.classList.add('btn')
+    editButtton.classList.add('btn-info')
+
+    const deleteButton = document.createElement('button');
+    deleteButton.innerText = 'delete'
+    deleteButton.classList.add('btn')
+    deleteButton.classList.add('btn-danger')
+
+
+    task.appendChild(taskLabel);
+    task.appendChild(editButtton);
+    task.appendChild(deleteButton);
+    taskContainer.appendChild(task);
+    
+    editButtton.addEventListener("click", editTodo)
+    deleteButton.addEventListener("click", deleteTodo)
+    
+    function deleteTodo(){
+     console.log("borrar tarea")
+     task.remove()
+    }
+
+    function editTodo(){
+      const todoEdit = prompt("Change your task");
+      taskLabel.innerText = todoEdit;
+    }
+  };
+  
+  createTodo(todoInput.value)
   todoInput.value = '';
 
-  if(createTodo){
-    const editButton = document.getElementById(`edit-buttons${index}`);
-    const deleteButton = document.getElementById("delete-button");
-    editButton.addEventListener("click", editTodo);
-    deleteButton.addEventListener("click", deleteTodo);
-  }
-  
-  index++;
 }
 
-function editTodo(){
-  const todoChange = prompt("Change your task");
-  console.log(todoChange);
-}
 
-function deleteTodo(){
-  todoList.removeChild(todoList.firstChild);
-}
 
-function createTodo(value){
-  const saludo = `<div>
-    <li class="todo-list">${value}</li>
-    <button id="edit-button${index}">Edit</button>
-    <button id="delete-button${index}">Delete</button>
-    </div>
-    `;
-  const $list = document.querySelector ('ul');
-  $list.insertAdjacentHTML('beforeend', saludo);
-}
